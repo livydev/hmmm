4e1f07d542d18fbc95de81e7aeec80c81e26e9d6
Dellas <dellasdp403@gmail.com>
2022-03-08 21:06:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse()
 global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
 
 global.db = new Low(
-  /https?:\/\//.test(opts['db'] || 'mongodb://mongo:jji8Kk0hMDU052jcKRre@containers-us-west-2.railway.app:7734') ?
+  /https?:\/\//.test(opts['db'] || '') ?
     new cloudDBAdapter(opts['db']) : /mongodb(\+srv)?:\/\//i.test(opts['db']) ?
       (opts['mongodbv2'] ? new mongoDBV2(opts['db']) : new mongoDB(opts['db'])) :
       new JSONFile(`${opts._[0] ? opts._[0] + '_' : ''}database.json`)
