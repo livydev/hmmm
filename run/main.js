25c9c9cb4c49a8814eef0747d23fa6d2dcf53b35
Dellas <dellasdp403@gmail.com>
2022-03-18 21:57:38 +0800

    Update main.js

--- run/main.js
+++ run/main.js
@@ -1,5 +1,5 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-import './config.js';
+import './run/config.js';
 
 import { createRequire } from "module"; // Bring in the ability to create the 'require' method
 import path, { join } from 'path'
