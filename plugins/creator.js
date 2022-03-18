50891a7891b1bac181be6b3e017ff3cac699e29c
Dellas <dellasdp403@gmail.com>
2022-03-18 08:39:25 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,8 +1,27 @@
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+import PhoneNumber from 'awesome-phonenumber'
+
+async function handler(m) {
+
+                let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+
+                    + 'VERSION:3.0\n' 
+
+                    + 'N:;Della;;;'
+
+                    + 'FN:Della\n' // full name
+
+                    + 'ORG:CloudBotOwner;\n' // the organization of the contact
+
+                    + 'TEL;type=CELL;type=VOICE;waid=6281224863098:+62 812-2486-3098\n' // WhatsApp ID + phone number
+
+                    + 'END:VCARD'
+
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Della', contacts: [{ vcard }] } }, { quoted: m })
+
 }
+
 handler.help = ['owner', 'creator']
+
 handler.tags = ['info']
 
 handler.command = /^(owner|creator)$/i
