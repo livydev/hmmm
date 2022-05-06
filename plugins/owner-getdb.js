14af4ae86a22b8c69f49a86cf8e903462d8dcb90
Dellas <dellasdp403@gmail.com>
2022-05-06 09:13:08 +0800

    Update owner-getdb.js

--- plugins/owner-getdb.js
+++ plugins/owner-getdb.js
@@ -1,8 +1,8 @@
 import fs from 'fs'
 let handler = async (m, { conn, text }) => {
-    m.reply('Tunggu Sebentar, Proses Getting File session.data.json')
-    let sesi = await fs.readFileSync('./database.json')
-    return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'session.data.json' }, { quoted: m })
+    m.reply('Tunggu Sebentar, Proses Getting File database.json')
+    let databasenya = await fs.readFileSync('./database.json')
+    return await conn.sendMessage(m.chat, { document: databasenya, mimetype: 'application/json', fileName: 'database.json' }, { quoted: m })
 }
 handler.help = ['getdb']
 handler.tags = ['host']
